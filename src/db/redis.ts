cdc3eb537c9fcca859cfdd40473a595a4737b9a4
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-01 11:28:16 +0000

    feat(db): init redis
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/db/redis.ts
@@ -0,0 +1,8 @@
+import Redis from 'ioredis';
+
+/**
+ * Get redis.
+ * @return {Redis}
+ */
+export const getRedis = (): Redis =>
+    new Redis(process.env.REDIS_URL ?? 'redis://:@localhost:6379');
