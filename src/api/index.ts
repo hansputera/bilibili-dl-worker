04c07c5e0f657c67aa908cbed33d045d47395778
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-01 14:11:58 +0000

    feat: reset
    Signed-off-by: GitHub <noreply@github.com>

--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import {App} from '@tinyhttp/app';
-import {v1App} from './v1.js';
-import {v2App} from './v2.js';
-
-export const apiApp = new App();
-
-apiApp.get('/', (_, res) => res.status(200).send('Welcome to the API!'));
-apiApp.route('/v1').use(v1App);
-apiApp.route('/v2').use(v2App);
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,13 @@
 import 'dotenv/config';
 
-import {App as TinyHttpApp} from '@tinyhttp/app';
-import {logger} from '@tinyhttp/logger';
-import {cors} from '@tinyhttp/cors';
-import {rateLimit} from '@tinyhttp/rate-limit';
-import {apiApp} from './api/index.js';
+import http from 'node:http2';
 
-const app = new TinyHttpApp({
-    noMatchHandler: (_, res) => {
-        return res
-            .status(404)
-            .send('The resource you are looking for does not exist.');
-    },
-    onError: (err, _, res) => {
-        res.setHeader('Error-Message', err.message);
-        return res.status(500).send('An error has occurred.');
-    },
+const server = http.createServer({
+    'maxSessionMemory': 1024 * 1024 * 1024,
 });
 
-app.use(
-    logger({
-        ip: true,
-        timestamp: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        emoji: true,
-    }),
-    cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    }),
-    rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 50, // limit each IP to 100 requests per windowMs
-        message: 'Too many requests from this IP, please try again later.',
-    }),
-);
-
-app.all('/', (_, res) => {
-    return res.status(201).send('OK!');
+server.on('request', (_, res) => {
+    res.end('OK!');
 });
 
-app.route('/api').use(apiApp);
-
-app.listen(parseInt(process.env.PORT!) || 3000);
+server.listen(parseInt(process.env.PORT!) || 3000);
