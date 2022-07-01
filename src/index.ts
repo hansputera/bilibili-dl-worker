6ead815bddef082ac7d31c868ac51d3ff9f114f5
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-01 14:47:05 +0000

    feat: init the process
    Signed-off-by: GitHub <noreply@github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import 'dotenv/config';
 
 import http from 'node:http';
+import {initProcess} from './process.js';
 
 const server = http.createServer();
+
+initProcess();
 
 server.on('request', (_, res) => {
     res.end('Hello world');
