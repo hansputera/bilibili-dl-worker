f0fdff840d2372273bfe4319c179bf214eb784ff
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-01 09:59:00 +0000

    chore: disable camelcase on line 40, and 45
    Signed-off-by: GitHub <noreply@github.com>

--- src/gapi.ts
+++ src/gapi.ts
@@ -10,10 +10,8 @@ export class GAPI {
 
     /**
      * @constructor
-     * @param {string[]} scopes - The scopes to request.
      */
-    constructor(private scopes: string[]) {
-        if (!Array.isArray(scopes)) throw new Error('Scopes must be an array');
+    constructor() {
         this.credentialsFilePath = path.resolve(
             new URL(import.meta.url).pathname,
             '../google_credentials.json',
@@ -26,7 +24,7 @@ export class GAPI {
 
     /**
      * Initialize the Google API client.
-     * @returns {Promise<google.auth.OAuth2>}
+     * @return {Promise<google.auth.OAuth2>}
      */
     async init() {
         const contents = JSON.parse(
@@ -39,15 +37,39 @@ export class GAPI {
 
         if (contents.err) throw new Error(contents.err);
 
+        // eslint-disable-next-line camelcase
         const {client_id, client_secret, redirect_uris} = contents;
         const oauth2Client = new google.auth.OAuth2(
             client_id,
             client_secret,
+            // eslint-disable-next-line camelcase
             redirect_uris[0],
         );
 
-        const token = await oauth2Client.getToken(contents.refresh_token);
-        oauth2Client.setCredentials(token.tokens);
+        if (
+            !fs.existsSync(
+                path.resolve(
+                    new URL(import.meta.url).pathname,
+                    '../google_tokens.json',
+                ),
+            )
+        )
+            throw new Error('Tokens file not found');
+
+        const tokens = JSON.parse(
+            await fs.promises
+                .readFile(
+                    path.resolve(
+                        new URL(import.meta.url).pathname,
+                        '../google_tokens.json',
+                    ),
+                    {encoding: 'utf8'},
+                )
+                .catch((err) => '{"err": "'.concat(err.message, '"}')),
+        );
+
+        if (tokens.err) throw new Error(tokens.err);
+        oauth2Client.setCredentials(tokens);
 
         return oauth2Client;
     }
