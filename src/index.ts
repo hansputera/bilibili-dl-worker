7665d610aab7102e83307fcc6f5e2453d078919a
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-01 08:07:58 +0000

    feat: set ratelimit max to 50
    Signed-off-by: GitHub <noreply@github.com>

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.use(
     }),
     rateLimit({
         windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // limit each IP to 100 requests per windowMs
+        max: 50, // limit each IP to 100 requests per windowMs
         message: 'Too many requests from this IP, please try again later.',
     }),
 );
