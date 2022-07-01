18b23f6aaa6ea8e9a70403cd59bd46b17634537c
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-01 07:41:07 +0000

    feat: add @tinyhttp
    Signed-off-by: GitHub <noreply@github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 import 'dotenv/config';
+
+import {App as TinyHttpApp} from '@tinyhttp/app';
+import {logger} from '@tinyhttp/logger';
+
+const app = new TinyHttpApp({
+    noMatchHandler: (_, res) => {
+        return res.status(404).send('Not found');
+    },
+});
+
+app.use(
+    logger({
+        ip: true,
+        timestamp: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        emoji: true,
+    }),
+);
+
+app.listen(parseInt(process.env.PORT!) || 3000);
