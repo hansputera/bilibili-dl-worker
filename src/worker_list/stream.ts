6cf1b0e16a005ac3f1c26ea7f9e3eca7347cbceb
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-02 08:31:25 +0000

    style: prettier
    Signed-off-by: GitHub <noreply@github.com>

--- src/worker_list/stream.ts
+++ src/worker_list/stream.ts
@@ -1,8 +1,9 @@
 import {WorkerManager} from '../workers/manager.js';
 
 export const streamWorker = async (manager: WorkerManager) => {
-    manager.createWorker('stream')
-    // Downloader job
+    manager
+        .createWorker('stream')
+        // Downloader job
         .register('downloader', async (job) => {
             console.log(`[Job: ${job.id}] Downloader`);
         });
