ad3fc90ac04fe69976119d1cc8248d8d86ebdbd3
hansputera <hanifdwyputrasembiring@gmail.com>
2022-07-03 08:57:31 +0000

    feat: add logger
    Signed-off-by: GitHub <noreply@github.com>

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,10 @@ server.on('request', (_, res) => {
     res.end('Hello world');
 });
 
+server.on('error', console.error);
+
+server.on('listening', () => {
+    console.log('Server listening to', server.address());
+});
+
 server.listen(parseInt(process.env.PORT!) || 3000, '0.0.0.0');
