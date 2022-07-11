53ee77779b0dbec5fa84617a510be36ecfb37928
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-11 11:12:40 +0000

    feat: configure the node-static
    Signed-off-by: GitHub <noreply@github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import 'dotenv/config';
 
 import http from 'node:http';
+import nodeStatic from 'node-static';
+import {cwd} from 'node:process';
+
 import {initProcess} from './process.js';
 
-const server = http.createServer();
-
-initProcess();
-
-server.on('request', (_, res) => {
-    res.end('Hello world');
+const staticServer = new nodeStatic.Server(`${cwd()}/downloads`, {
+    cache: 7200,
+});
+const server = http.createServer(async (req, res) => {
+    req.addListener('end', async () => {
+        staticServer.serve(req, res, (err) => {
+            if (err) {
+                res.statusCode = (err as unknown as {status: number}).status;
+                res.end(err.message);
+            }
+        });
+    }).resume();
 });
 
+initProcess();
 server.on('error', console.error);
 
 server.on('listening', () => {
